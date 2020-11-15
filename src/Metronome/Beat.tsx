1819aeab61e226575faaa55fb6b7b8d3c7701883
Will Moritz <will@crowdfavorite.com>
2020-11-14 18:13:11 -0800

    better use of css modules

--- src/Metronome/Beat.tsx
+++ src/Metronome/Beat.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import InputRange from "./InputRange";
 import BeatMarker from "./BeatMarker";
+import style from "../styles/beat.module.scss";
 
 export interface BeatState {
 	volume: number;
@@ -10,12 +11,12 @@ export interface BeatState {
 type BeatProps = {
 	volume: number;
 	on: boolean;
-	onVolumeUpdate: (val: number) => void;
+	onVolumeUpdate: (val: number) => void
 };
 
 function Beat({ volume, on, onVolumeUpdate }: BeatProps) {
 	return (
-		<div className='beat'>
+		<div className={style.beat}>
 			<BeatMarker on={on} />
 			<InputRange orientation='vertical' value={volume} min={0} max={100} onChange={onVolumeUpdate} />
 		</div>
