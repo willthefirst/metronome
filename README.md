be450ebe8d1b95f90a05b06eb3abf42c43a83abd
Will Moritz <will@crowdfavorite.com>
2020-12-03 10:35:11 -0800

    works but feels dangerous

--- README.md
+++ README.md
@@ -5,18 +5,11 @@
     - [x] Write a test for the start/stop button
     - [ ] Use WebAudio to get a click going
         - [x] First, figure out how you'll want to work with audiocontext via play/pause.
-        - Maybe submit a PR to have them clarify this (Is it passed in from a parent via props? If so, it probably isn’t state.)
-        - [ ] Next up: https://reactjs.org/docs/thinking-in-react.html#step-3-identify-the-minimal-but-complete-representation-of-ui-state
-        - [WebAudio resource](https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Advanced_techniques#Playing_the_audio_in_time)
-        - We now can play sounds.
-        - On START
-            - Set beat to 1
-            - setinterval/webaudio timer to fire according to BPM, on every fire:
-                - rotate beat in state, and when this happens
-                    - play the sound
-                    - current beat should be highlighted visually
-    - [ ] Hook up BPM
-    - [ ] Hook up gain nodes to sliders
+        - [ ] Play accent on every first beat
+            - current bug is that because of the dependecy array iin `useEffect()`, we're not updating current beat
+        - [ ] Organize functions/code-splitting
+        - [ ] Current beat is highlitted visually
+        - [ ] Hook up gain nodes to sliders
 - [ ] Polish UI
     - [ ] Move colors somewhere sensible
     - [ ] BIG bpm text
--- src/Metronome/index.tsx
+++ src/Metronome/index.tsx
@@ -18,17 +18,27 @@ let settings = {
 	isPlaying: false,
 	bpm: 88,
 	beats: defaultBeats,
-	currentBeatIndex: 1
+	currentBeatIndex: 0
 };
 
 let audioCtx: AudioContext | undefined = undefined;
+let samples: AudioBufferSourceNode[] = [];
+let nextBeatTime: number = 0;
+const notesInQueue: { note: number; time: number }[] = [];
+let timerID: number | undefined;
+// Update state progressively
+let lookahead = 25.0; // How frequently to call scheduling function (in milliseconds)
+let scheduleAheadTime = 0.1; // How far ahead to schedule audio (sec)
+
+let audioBuffers: [] | AudioBuffer[] = [];
 
 function Metronome() {
 	const [isPlaying, setPlaying] = useState(settings.isPlaying);
+	const [bpm, setBPM] = useState(settings.bpm);
+	const [currentBeatIndex, setCurrentBeat] = useState(settings.currentBeatIndex);
+	const [beats, setBeats] = useState(settings.beats);
 
-	useEffect(() => {
-		isPlaying ? start() : stop();
-	});
+	console.log(`Top level: ${currentBeatIndex}`);
 
 	async function setupSamples(audioContext: AudioContext): Promise<AudioBuffer[]> {
 		const samples = [`click.wav`, `accent.wav`];
@@ -42,39 +52,79 @@ function Metronome() {
 		return audioBuffers;
 	}
 
-	const loadSamples = async (): Promise<AudioBufferSourceNode[]> => {
+	const loadSamples = async () => {
 		audioCtx = createContext();
-
-		const audioBuffers = await setupSamples(audioCtx);
-		const sources = audioBuffers.map((buffer) => {
-			const sampleSource = audioCtx!.createBufferSource();
-			sampleSource.buffer = buffer;
-			sampleSource.connect(audioCtx!.destination);
-			return sampleSource;
-		});
-		return sources;
+		audioBuffers = await setupSamples(audioCtx);
 	};
 
+	const playSound = (buffer: AudioBuffer | null) => {
+		const sampleSource = audioCtx!.createBufferSource();
+		sampleSource.buffer = buffer;
+		sampleSource.connect(audioCtx!.destination);
+		sampleSource.start();
+		return sampleSource;
+	};
+
+	function incrementBeat(prevBeat: number) {
+		const secondsPerBeat = 60.0 / bpm;
+
+		nextBeatTime += secondsPerBeat; // Add beat length to last beat time
+
+		// Advance the beat number, wrap to zero
+		if (prevBeat >= beats.length - 1) {
+			return 0;
+		} else {
+			return prevBeat + 1;
+		}
+	}
+
+	function scheduleNote(beatNumber: number, time: number) {
+		// push the note on the queue, even if we're not playing.
+		notesInQueue.push({ note: beatNumber, time: time });
+		if (beatNumber === 0) {
+			playSound(audioBuffers[1]); // beat sound
+		} else {
+			playSound(audioBuffers[0]); // accent sound
+		}
+	}
+
+	function scheduler() {
+		// While there are notes that will need to play before the next interval, schedule them and advance the pointer.
+		while (nextBeatTime < audioCtx!.currentTime + scheduleAheadTime) {
+			setCurrentBeat((prevBeat) => {
+				scheduleNote(prevBeat, nextBeatTime);
+				return incrementBeat(prevBeat);
+			});
+		}
+		timerID = window.setTimeout(scheduler, lookahead);
+	}
 
 	const start = async () => {
-		let samples: void | AudioBufferSourceNode[] = [];
+		console.log("Start");
 		// Initialize audio if needed
 		if (!audioCtx) {
-			samples = await loadSamples();
+			await loadSamples();
+		} // check if context is in suspended state (autoplay policy)
+		else if (audioCtx.state === "suspended") {
+			audioCtx.resume();
 		}
 
-		samples[1].start();
-
-		// Update state progressively
+		nextBeatTime = audioCtx!.currentTime;
+		scheduler();
 	};
 
 	const stop = () => {
-		// Stop this shit
+		window.clearTimeout(timerID);
 	};
 
 	const handlePlayToggle = async () => {
 		setPlaying(!isPlaying);
 	};
+
+	useEffect(() => {
+		console.log("effect!");
+		isPlaying ? start() : stop();
+	}, [isPlaying]);
 
 	return (
 		<div className={layout.container}>
